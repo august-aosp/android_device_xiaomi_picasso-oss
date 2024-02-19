e9a4300596948db94e8c9f242c460053ea7675a4
johnmart19 <johnivan19999@gmail.com>
2024-02-19 06:27:37 +0200

    picasso: Camera: Enable camera override format
    Introduced in https://github.com/LineageOS/android_hardware_interfaces/commit/734d45c1a76667d8d02f2b3f3a165f630c3e4092
    
    Previously was enabled by default
    
    Signed-off-by: hoang1007 <hoangvuhuy1007@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,6 +14,9 @@ include device/xiaomi/sm8250-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/xiaomi/picasso
 
+# Camera
+TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED := true
+
 # Display
 TARGET_SCREEN_DENSITY := 420
 
