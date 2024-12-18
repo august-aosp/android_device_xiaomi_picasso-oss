339899a593b5557494bdf4d59272a59d18185986
hoang1007 <hoangvuhuy1007@gmail.com>
2024-12-18 18:25:45 +0700

    picasso: Welcome Pixelage
    Signed-off-by: hoang1007 <hoangvuhuy1007@gmail.com>

--- pixelage_picasso.mk
+++ pixelage_picasso.mk
@@ -8,20 +8,19 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common DerpFest stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+# Inherit some common PixelAge stuff.
+$(call inherit-product, vendor/pixelage/config/common_full_phone.mk)
 
 # Inherit from picasso device
 $(call inherit-product, device/xiaomi/picasso/device.mk)
 
-PRODUCT_NAME := derp_picasso
+PRODUCT_NAME := pixelage_picasso
 PRODUCT_DEVICE := picasso
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi K30 5G
 
 TARGET_BOOT_ANIMATION_RES := 1080
-DERP_BUILDTYPE := Official
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
