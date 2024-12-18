2cdc48bc6b9eb32312dae04f0498499dce755317
hoang1007 <hoangvuhuy1007@gmail.com>
2024-12-18 18:25:45 +0700

    picasso: Welcome Pixelage
    Signed-off-by: hoang1007 <hoangvuhuy1007@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_picasso.mk
+    $(LOCAL_DIR)/pixelage_picasso.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_picasso-user \
-    derp_picasso-userdebug \
-    derp_picasso-eng
+    pixelage_picasso-user \
+    pixelage_picasso-userdebug \
+    pixelage_picasso-eng
