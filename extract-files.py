aa0d6a0a6107d276376185994b0bdbc864eb95f0
hoang1007 <hoangvuhuy1007@gmail.com>
2024-12-18 18:22:09 +0700

    picasso: Switch to python extract-utils
    Signed-off-by: hoang1007 <hoangvuhuy1007@gmail.com>

---
+++ extract-files.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.file import File
+from extract_utils.fixups_blob import (
+    BlobFixupCtx,
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    "vendor/xiaomi/sm8250-common",
+    "hardware/qcom-caf/common/libqti-perfd-client",
+    "hardware/qcom-caf/sm8250",
+    "hardware/qcom-caf/wlan",
+    "hardware/xiaomi",
+    "vendor/qcom/opensource/display",
+]
+
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    'vendor/lib/hw/audio.primary.picasso.so': blob_fixup()
+        .binary_regex_replace(b'/vendor/lib/liba2dpoffload\.so', b'liba2dpoffload_picasso.so\x00\x00\x00\x00'),
+    'vendor/lib64/camera/components/com.mi.node.watermark.so': blob_fixup()
+        .add_needed('libpiex_shim.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'picasso',
+    'xiaomi',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+    check_elf=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(module, 'sm8250-common')
+    utils.run()
