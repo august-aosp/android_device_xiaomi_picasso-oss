b41d82fd41c8f479a9d0dfe659b7609a286d22fd
hoang1007 <hoangvuhuy1007@gmail.com>
2025-07-10 21:38:13 +0700

    Revert "picasso: Rename prebuilt audio HAL and libs"
    No longer needed cuz we're shipping prebuilt audio HAL.
    
    This reverts commit d91dabd4fbfd6e1a45fae82ba3bdbd40ee9072f4.

--- extract-files.py
+++ extract-files.py
@@ -39,9 +39,7 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
-    'vendor/lib/hw/audio.primary.picasso.so': blob_fixup()
-        .binary_regex_replace(b'/vendor/lib/liba2dpoffload.so', b'liba2dpoffload_picasso.so\x00\x00\x00\x00'),
-    ('vendor/lib/hw/audio.primary.picasso.so', 'vendor/lib/libaudioroute_ext.so'): blob_fixup()
+    ('vendor/lib/hw/audio.primary.lito.so', 'vendor/lib/libaudioroute_ext.so'): blob_fixup()
         .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),
     'vendor/lib64/camera/components/com.mi.node.watermark.so': blob_fixup()
         .add_needed('libpiex_shim.so'),
