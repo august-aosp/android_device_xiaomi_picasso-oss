f84cfdcf03c8670f3ff9dc3a2f5530162df77514
johnmart19 <johnivan19999@gmail.com>
2025-07-24 06:58:14 +0300

    picasso: Move Camera Override Format from Reserved to soong_config_set
    Fixes error:
    vendor/lineage/config/BoardConfigSoong.mk:39:
    error:
    TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED is deprecated, please migrate to soong_config_set,camera,override_format_from_reserved.
    
    Signed-off-by: hoang1007 <hoangvuhuy1007@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,9 +14,6 @@ include device/xiaomi/sm8250-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/xiaomi/picasso
 
-# Camera
-TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED := true
-
 # Display
 TARGET_SCREEN_DENSITY := 420
 
